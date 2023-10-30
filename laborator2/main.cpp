dd15ec4ba8351b583467fcb3830463f613df699b
eva.simpetru <eva.simpetru@esolutions.ro>
2023-10-30 17:36:05 +0200

    Resolve lab2 - parcurgere postordine, nr frunze recurvi & inaltime arbore recursiv

--- laborator2/main.cpp
+++ laborator2/main.cpp
@@ -4,10 +4,10 @@
  */
 #include <iostream>
 #include <cstdlib>
+
 using namespace std;
 
-struct NodArb
-{
+struct NodArb {
     int info;
     NodArb *stang;
     NodArb *drept;
@@ -15,93 +15,113 @@ struct NodArb
 
 //typedef struct NodArb* NodArbPtr;
 
-NodArb* creaza_arbore();
+NodArb *creaza_arbore();
 
 void tiparire_arbore(NodArb *rad);
 
-void inordine(NodArb *rad)
-{
-    if (rad != NULL)
-    {
+void inordine(NodArb *rad) {
+    if (rad != NULL) {
         inordine(rad->stang);
-        cout << rad ->info << " ";
-        inordine(rad -> drept);
+        cout << rad->info << " ";
+        inordine(rad->drept);
     }
 }
 
-void preordine(NodArb *rad)
-{
-    if (rad != NULL)
-    {
-        cout << rad ->info << " ";
+void preordine(NodArb *rad) {
+    if (rad != NULL) {
+        cout << rad->info << " ";
         preordine(rad->stang);
-        preordine(rad -> drept);
+        preordine(rad->drept);
     }
 }
 
-int nr_noduri (NodArb *rad)
-{
+void postordine(NodArb *rad) {
+    if (rad != NULL) {
+        postordine(rad->stang);
+        postordine(rad->drept);
+        cout << rad->info << " ";
+    }
+}
+
+int nr_noduri(NodArb *rad) {
     if (rad != NULL)
         return nr_noduri(rad->stang) + nr_noduri(rad->drept) + 1;
     else return 0;
 }
-int main()
-{
-    NodArb* rad=NULL;
-    cout<< "Construim arborele...\n";
-    rad = creaza_arbore();
-    cout<<"\n\nTiparire arbore\n\n";
-    tiparire_arbore(rad);
-    cout <<endl<<endl;
 
-    cout << "INORDINE:" ;
+int nr_frunze(NodArb *rad) {
+    if (rad == NULL) {
+        return 0;
+    }
+
+    if (rad->stang == NULL && rad->drept == NULL) {
+        return 1;
+    }
+    return nr_frunze(rad->stang) + nr_frunze(rad->drept);
+}
+
+int inaltime(NodArb *rad) {
+    if (rad == NULL || (rad->drept == NULL && rad->stang == NULL)) {
+        return 0;
+    }
+    return 1 + max(inaltime(rad->stang), inaltime(rad->drept));
+}
+
+int main() {
+    NodArb *rad = NULL;
+    cout << "Construim arborele...\n";
+    rad = creaza_arbore();
+    cout << "\n\nTiparire arbore\n\n";
+    tiparire_arbore(rad);
+    cout << endl << endl;
+
+    cout << "INORDINE: ";
     inordine(rad);
     cout << endl;
-    cout << "PREORDINE:" ;
+    cout << "PREORDINE: ";
     preordine(rad);
     cout << endl;
+    cout << "POSTORDINE: ";
+    postordine(rad);
+    cout << endl;
 
-    cout<<"Numarul de noduri este: " << nr_noduri(rad)<<endl;
+    cout << "Numarul de noduri este: " << nr_noduri(rad) << endl;
+    cout << "Numarul de frunze este: " << nr_frunze(rad) << endl;
+    cout << "Inaltimea arborelui binar este: " << inaltime(rad) << endl;
 
 
     return 0;
 }
 
 
-
-NodArb* creaza_arbore()
-{
-    NodArb* rad;
+NodArb *creaza_arbore() {
+    NodArb *rad;
     char c;
-    cout<<"Introduceti un nod (Y/N)? ";
+    cout << "Introduceti un nod (Y/N)? ";
     // rewind(stdin);
-    cin>>c;
-    if(c=='y'||c=='Y')
-    {
-        rad = new  NodArb;
-        cout<<"Info radacina: ";
-        cin >> rad -> info;
-        cout<<"\nNodul stang al lui " << rad -> info <<endl;
-        rad -> stang = creaza_arbore();
-        cout<<"\nNodul drept al lui " << rad -> info <<endl;
-        rad -> drept = creaza_arbore();
-    }
-    else rad=NULL;
+    cin >> c;
+    if (c == 'y' || c == 'Y') {
+        rad = new NodArb;
+        cout << "Info radacina: ";
+        cin >> rad->info;
+        cout << "\nNodul stang al lui " << rad->info << endl;
+        rad->stang = creaza_arbore();
+        cout << "\nNodul drept al lui " << rad->info << endl;
+        rad->drept = creaza_arbore();
+    } else rad = NULL;
     return rad;
 }
 
 
-void tiparire_arbore(NodArb* rad)
-{
-    if(rad==NULL)
-        cout<<"NULL\n";
-    else
-    {
-        cout<<"rad = "<< rad ->info<<endl;
-        cout<<"Subarbore stang al lui  " << rad -> info <<endl;
-        tiparire_arbore(rad -> stang);
-        cout<<"Subarbore drept al lui " << rad -> info <<endl;
-        tiparire_arbore(rad -> drept);
+void tiparire_arbore(NodArb *rad) {
+    if (rad == NULL)
+        cout << "NULL\n";
+    else {
+        cout << "rad = " << rad->info << endl;
+        cout << "Subarbore stang al lui  " << rad->info << endl;
+        tiparire_arbore(rad->stang);
+        cout << "Subarbore drept al lui " << rad->info << endl;
+        tiparire_arbore(rad->drept);
     }
 }
 
