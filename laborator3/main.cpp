70066ad705c2f5e2741b400f98212f0c80129090
eva.simpetru <eva.simpetru@esolutions.ro>
2024-01-13 14:38:19 +0200

    Bubble sort

--- laborator3/main.cpp
+++ laborator3/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int n, v[100];
 
-void afisareVector(int v[], int n) {
+void displayVector() {
     cout << "Vectorul sortat: [" << v[0];
     for (int i = 1; i < n; ++i) {
         cout << " " << v[i];
@@ -12,7 +12,7 @@ void afisareVector(int v[], int n) {
     cout << "]\n";
 }
 
-void citireVector() {
+void readVector() {
     cout << "Introduceti nr elemente: ";
     cin >> n;
 
@@ -22,7 +22,7 @@ void citireVector() {
     }
 }
 
-void selectionSort(int v[], int n) {
+void selectionSort() {
 
     for (int j = 0; j < n - 1; j++) {
         int min = v[j];
@@ -38,25 +38,77 @@ void selectionSort(int v[], int n) {
         }
     }
 
-    afisareVector(v, n);
+    displayVector();
+}
+
+void insertionSort() {
+    for (int i = 1; i < n; i++) {
+        int j = i - 1;
+        while (j >= 0 && v[j + 1] < v[j]) {
+            swap(v[j + 1], v[j]);
+            j--;
+        }
+    }
+
+    displayVector();
+
+    // Best case: sorted ASC vector => n - 1 comparisons -> O(n) complexity
+    // Worst case: sorted DESC vector => n(n - 1) / 2 comparisons -> O(n^2) complexity
+}
+
+void bubbleSort() {
+    bool hasChanged;
+    int lastElem = n;
+    do {
+        hasChanged = false;
+        for (int i = 0; i < lastElem - 1; ++i) {
+            if (v[i] > v[i + 1]) {
+                hasChanged = true;
+                swap(v[i], v[i + 1]);
+            }
+        }
+        lastElem--;
+    } while (hasChanged);
+
+    displayVector();
+
+    // Best case: sorted ASC vector => n - 1 comparisons -> O(n) complexity
+    // Worst case: sorted DESC vector => n(n - 1) / 2 comparisons -> O(n^2) complexity
+}
+
+void quickSort() {
+
+    displayVector();
 }
 
 int main() {
 
 
-    citireVector();
+    readVector();
 
     int x;
     cout << "\n-----------------------------------\n";
     cout << "Alegeti metoda de sortare:\n";
-    cout << "|  1. Selection sort - O(n^2)\n";
+    cout << "|  1. Selection sort   - O(n^2)\n";
+    cout << "|  2. Insertion sort   - O(n^2)\n";
+    cout << "|  3. Bubble sort      - O(n^2)\n";
+    cout << "|  4. Quick sort       - O(n^2)\n";
     cout << "-----------------------------------\n";
     cin >> x;
     cout << endl;
 
     switch (x) {
         case 1:
-            selectionSort(v, n);
+            selectionSort();
+            break;
+        case 2:
+            insertionSort();
+            break;
+        case 3:
+            bubbleSort();
+            break;
+        case 4:
+            quickSort();
             break;
         default:
             return 0;
